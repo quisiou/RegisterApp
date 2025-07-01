c4edde12bb84c3830ea3718d835b15135cd3d0ae
QuiSioU <marco.casteleiro@gmail.com>
2025-07-01 02:05:22 +0200

    starting to improve front end

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from src.app import App
+from src.bootstrapApp import App as TkbApp
 from src.dataManager import Manager
+
+from utils.parameters import WINDOW_WIDTH, WINDOW_HEIGHT
 
 if __name__ == '__main__':
     Manager.setup()
-    app = App(custom_theme='dark-blue')
+    
+    # app = App(custom_theme='dark-blue')
+
+    app = TkbApp(
+        title='Clocker',
+        size=(WINDOW_WIDTH, WINDOW_HEIGHT),
+        minsize=(WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2)
+    )
     app.mainloop()
